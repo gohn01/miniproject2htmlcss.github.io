bf3f001549799413910a9de4e311bb0707201dcc
ShairaHonrejas <honrejasshaira@gmail.com>
2023-03-29 16:15:06 +0800

    added new file and change the navbar in style.css

---
+++ services.js
@@ -0,0 +1,6 @@
+//Popover in Information Icon
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+
