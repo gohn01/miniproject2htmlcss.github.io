65a1bd0d64ed3d401c85b8618e84bd48c518ce46
Alexis John Perez <alexisjohnperez99@gmail.com>
2023-03-31 19:51:59 +0800

    Navbar

---
+++ js/services.js
@@ -0,0 +1,7 @@
+//Popover in Information Icon
+let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+
+
---
+++ js/submain.js
@@ -0,0 +1,28 @@
+//Popover in Information Icon
+    // let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    // let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    //   return new bootstrap.Popover(popoverTriggerEl)
+    // })
+
+
+
+// Hamburger menu
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n. 
+    addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    }))
+
+
+    // window.addEventListener("scroll", ()=> {
+    //     let nav = document.querySelector("nav");
+    //     nav.classList.toggle("sticky", window.scrollY > 0)
+    // })
