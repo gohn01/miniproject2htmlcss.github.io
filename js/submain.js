ca129d464713ffce7ec65d01a64e208ebf479707
Alexis John Perez <alexisjohnperez99@gmail.com>
2023-04-02 20:19:38 +0800

    Hamburger Menu

--- js/submain.js
+++ js/submain.js
@@ -1,25 +1,11 @@
 //Popover in Information Icon
-    let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
     let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
       return new bootstrap.Popover(popoverTriggerEl)
-    })
+    });
 
 
 
-// Hamburger menu
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
-
-document.querySelectorAll(".nav-link").forEach(n => n. 
-    addEventListener("click", () => {
-        hamburger.classList.remove("active");
-        navMenu.classList.remove("active");
-    }))
 
 
     // window.addEventListener("scroll", ()=> {
